5a98e13459caef1b19caec873754cdff9e3e5f00
DonSebastian <donsebastian2421@gmail.com>
2023-11-26 13:44:05 -0500

    Sample input final

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+from app import app
+
+class BasicTestCase(unittest.TestCase):
+
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_content(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertTrue(b'Welcome' in response.data)
+
+if __name__ == '__main__':
+    unittest.main()
